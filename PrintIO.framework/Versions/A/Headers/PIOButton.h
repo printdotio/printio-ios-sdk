fd9a565d8489d894a60127f52cf92c253ec8a46b
perisicboro <tengai@Boros-MacBook-Pro.local>
2014-09-30 21:47:19 +0200

    bug fixes

--- PrintIO.framework/Versions/A/Headers/PIOButton.h
+++ PrintIO.framework/Versions/A/Headers/PIOButton.h
@@ -6,7 +6,7 @@
 //  Copyright (c) 2014 HelloPics. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 #import "PIOPublicConstants.h"
 
 @interface PIOButton : NSObject
