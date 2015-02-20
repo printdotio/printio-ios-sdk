5dea3e5021954005da9f31ce857d0e4775552418
perisicboro <tengai@Boros-MacBook-Pro.local>
2015-02-20 17:57:51 +0100

    1.0.48 bug fixes

---
+++ PrintIO.framework/Versions/A/Headers/PaymentOptions.h
@@ -0,0 +1,20 @@
+//
+//  PaymentOptions.h
+//  PrintIO
+//
+//  Created by Nikola Markovic on 10/1/14.
+//  Copyright (c) 2014 PrintIO. All rights reserved.
+//
+
+#import <CoreFoundation/CoreFoundation.h>
+
+typedef NSUInteger PaymentOptions;
+
+typedef enum : NSUInteger {
+    PaymentOptionPayPal = 0x1,
+    PaymentOptionCreditCard = 0x10,
+    PaymentOptionApplePay = 0x100,
+    PaymentOptionAll = 0x110
+} PaymentOption;
+
+BOOL paymentOptionSupported(PaymentOptions options, PaymentOption option);
