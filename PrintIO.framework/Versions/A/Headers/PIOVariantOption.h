3436c0b54a8ed7631370451c0b93a6dae322ebb0
perisicboro <tengai@retina15s-MacBook-Pro.local>
2015-12-08 18:44:37 +0100

    sdk update to v1.6.0(25)

---
+++ PrintIO.framework/Versions/A/Headers/PIOVariantOption.h
@@ -0,0 +1,20 @@
+//
+//  PIOVariantOption.h
+//  PrintIO
+//
+//  Created by Tengai on 5/2/14.
+//  Copyright (c) 2014 PrintIO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PIOVariantOption : NSObject
+
+@property (nonatomic, assign) int productId;
+@property (nonatomic, strong) NSString *optionId;
+@property (nonatomic, strong) NSString *valueId;
+@property (nonatomic, strong) UIColor *color;
+
+- (id)initWithProductId:(int)productId optionId:(NSString *)optionId valueId:(NSString *)valueId;
+
+@end
