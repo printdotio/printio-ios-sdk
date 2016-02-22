6e43069d7db070e0418afac1797d6e45df4a04dc
perisicboro <tengai@retina15s-MacBook-Pro.local>
2016-02-22 18:37:33 +0100

    sdk update v1.7.4(30)

---
+++ PrintIO.framework/Versions/A/Headers/PIOPhotoSourceItemImageDownloader.h
@@ -0,0 +1,30 @@
+//
+//  PhotoSourceItemImageDownloader.h
+//  PrintIO
+//
+//  Created by Boro Perisic Bocas on 1/14/15.
+//  Copyright (c) 2015 HelloPics. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "PIOPhotoSourceItem.h"
+#import "PIOPhotoSource.h"
+
+// Image downloader protocol
+
+@protocol PIOImageDownloader <NSObject>
+
+-(void)downloadImageForItem:(id<PIOPhotoSourceItem>)item inPhotoSource:(id<PIOPhotoSource>)photoSource isThumbnail:(BOOL)isThumbnail withCompletionHandler:(PIOImageDownloadCompletionHandler)imageCompletionHandler;
+
+@end
+
+typedef void(^PIOPhotoSourceItemImageDownloaderBlock)(id<PIOPhotoSourceItem>, id<PIOPhotoSource>, BOOL thumbnail, PIOImageDownloadCompletionHandler imageCompletionHandler);
+
+// default implementation
+@interface PIOPhotoSourceItemImageDownloader : NSObject<PIOImageDownloader>
+
++(instancetype)imageDownloaderWithBlock:(PIOPhotoSourceItemImageDownloaderBlock)imageDownloaderBlock;
+
+-(void)downloadImageForItem:(id<PIOPhotoSourceItem>)item inPhotoSource:(id<PIOPhotoSource>)photoSource isThumbnail:(BOOL)isThumbnail withCompletionHandler:(PIOImageDownloadCompletionHandler)imageCompletionHandler;
+
+@end
