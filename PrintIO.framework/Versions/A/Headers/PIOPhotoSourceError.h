80d57e76850a8074cdd1198a4ea3f6a912a8c188
perisicboro <tengai@retina15s-MacBook-Pro.local>
2016-04-14 11:31:28 +0200

    sdk update v1.7.22(32)

---
+++ PrintIO.framework/Versions/A/Headers/PIOPhotoSourceError.h
@@ -0,0 +1,30 @@
+//
+//  PhotoSourceError.h
+//  PrintIO
+//
+//  Created by Boro Perisic Bocas on 12/29/14.
+//  Copyright (c) 2014 HelloPics. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+typedef enum : NSUInteger {
+    PhotoSourceErrorTypeNone,
+    PhotoSourceErrorTypeNotLoggedIn,
+    PhotoSourceErrorTypeInvalidData,
+    PhotoSourceErrorTypeInvalidCredentials,
+    PhotoSourceErrorTypeAuthenticationError,
+    PhotoSourceErrorTypeConnectionError,
+    PhotoSourceErrorTypeUnknown
+} PhotoSourceErrorType;
+
+@interface PIOPhotoSourceError : NSObject
+
++(PIOPhotoSourceError*)errorWithMessage:(NSString*)message;
++(PIOPhotoSourceError*)errorWithErrorType:(PhotoSourceErrorType)type;
++(PIOPhotoSourceError*)errorWithErrorType:(PhotoSourceErrorType)type message:(NSString*)message;
+
+@property(nonatomic, strong) NSString* message;
+@property(nonatomic, assign) PhotoSourceErrorType type;
+
+@end
